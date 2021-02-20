59ce7186a1a01be3a58405347241d1a4af5b9522
Matt Jones <885417+jonesmac@users.noreply.github.com>
2021-02-20 21:45:51 +0000

    add backup to s3 script

---
+++ s3_backup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# A script to handle backing up bitwarden backups to s3 via cron
+# Be sure you set the following variables in the crontab:
+# PATH, BITWARDEN_BACKUPS_DIR, BITWARDEN_BACKUPS_BUCKET, LOG_FILE
+
+echo 'Initiating backup to s3' >> ${LOG_FILE}
+
+aws s3 sync ${BITWARDEN_BACKUPS_DIR} s3://${BITWARDEN_BACKUPS_BUCKET} --include "db.*" >> ${LOG_FILE} 2>&1
+
+echo 'Cron Complete' >> ${LOG_FILE}
